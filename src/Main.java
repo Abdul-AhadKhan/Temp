b1617981c11171c58a421cfeac593b586d76f31a
Abdul-AhadKhan <ahadravi571@gmail.com>
2023-11-27 19:16:33 +0500

    Second Commit By Ahad

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         for (int i = 0; i < 10; i++){
-            System.out.println("Hello World");
+            System.out.println("Hello World. This is the " + (i + 1) + " time printing hello world");
         }
     }
 }
